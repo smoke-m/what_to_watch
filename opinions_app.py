280bc08c16906346c0549444c9b95b1f11eb7fdc
Михаил Миноцкий <smoke-m@yandex.com>
2023-11-03 18:41:51 +0300

    hz

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+# from pprint import pprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Подключается БД SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Задаётся конкретное значение для конфигурационного ключа
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Создаётся экземпляр класса SQLAlchemy и передаётс
+# в качестве параметра экземпляр приложения Flask
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
+    # pprint(app.config)
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
